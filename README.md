a13f781f442a397d69e673482dd05dc955861b58
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-04-21 22:21:37 -0300

    exemplo de codigo aula 1 - template literals

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# javascript-iv-studies-obc
-Este repositório reúne materiais de estudo sobre JavaScript, abordando os tópicos avançados da linguagem.
+# Estudos de JavaScript
---
+++ aula-1-template-literals/script.js
@@ -0,0 +1,12 @@
+const nomeMessias = 'Jesus'
+
+console.log(`Eu sou ${nomeMessias}, o caminho, a verdade e a vida.`)
+
+const YHWH = 'Eu Sou'
+const Deus = 'Deus'
+
+console.log(`¹³ Então disse Moisés a Deus: Eis que quando eu for aos filhos de Israel, e lhes disser: O ${Deus} de vossos pais me enviou a vós; e eles me disserem: Qual é o seu nome? Que lhes direi?
+¹⁴ E disse ${Deus} a Moisés: Eu Sou o que Sou. Disse mais: Assim dirás aos filhos de Israel: ${YHWH} me enviou a vós.
+¹⁵ E ${Deus} disse mais a Moisés: Assim dirás aos filhos de Israel: O Senhor ${Deus} de vossos pais, o ${Deus} de Abraão, o ${Deus} de Isaque, e o ${Deus} de Jacó, me enviou a vós; este é meu nome eternamente, e este é meu memorial de geração em geração. 
+
+Êxodo 3:13-15`)
