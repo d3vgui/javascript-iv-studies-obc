2aadbd94d780111631f9696909f503eae08bde61
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-04-24 21:17:27 -0300

    exemplo de código aula 5 - operador de encadeamento opcional

---
+++ aula-5-operador-de-encadeamento-opcional/script.js
@@ -0,0 +1,30 @@
+const obj = {
+  a: {
+    b:{
+      c: 42
+    }
+  }
+}
+
+const user = {
+  name: "John Doe",
+  email: "doejohn@email.com",
+  friends: [{
+    name: "Mary",
+    address:{
+      street: "Some Street",
+      number: 89
+    }
+  }],
+  age: 42,
+  phone: {
+    countryCode: "+55",
+    ddd: "22",
+    number: "999999999"
+  }
+}
+
+console.log(user.friends[0].phone?.ddd)
+console.log(user?.brothers?.length)
+
+console.log(user.brothers?.[5].name)
